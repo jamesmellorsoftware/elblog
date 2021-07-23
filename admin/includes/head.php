f6848245a0c38227ab975a57b879eabcbbcaf9e5
James Mellor <mellor29j@gmail.com>
2021-07-23 16:18:08 +0200

    Put scripts in header

--- admin/includes/head.php
+++ admin/includes/head.php
@@ -21,7 +21,6 @@ if (!userIsAdmin() && !userIsWriter()) header("Location: ../index.php");
 	<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
-	<link rel="stylesheet" href="assets/vendor/chartist/css/chartist-custom.css">
 	<!-- MAIN CSS -->
 	<link rel="stylesheet" href="assets/css/main.css">
 	<!-- USER CSS DELETE AFTER -->
@@ -30,6 +29,10 @@ if (!userIsAdmin() && !userIsWriter()) header("Location: ../index.php");
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
 	<!-- JQUERY -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="assets/scripts/klorofil-common.js"></script>
+	<script src="assets/scripts/user_added_scripts.js"></script>
 </head>
 
 <body>
